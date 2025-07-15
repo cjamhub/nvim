5920273904751154284a73480eed7c845c8067f7
cjamhub <cjamchen@gmail.com>
2025-07-15 11:04:14 +0100

    update telescope diagnostics list preview style

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,49 +1,47 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'make',          -- optional native FZF sorter
-    },
-    config = function()
-        local telescope = require('telescope')
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.8",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "make", -- optional native FZF sorter
+	},
+	config = function()
+		local telescope = require("telescope")
 
-        telescope.setup {
-            defaults = {
-                path_display = { 'smart' },
-                layout_config = {
-                    width = 0.9,
-                    height = 0.9,
-                    preview_width = 0.6,
-                },
-                wrap_results = true,
-            },
-            pickers = {
-                diagnostics = {
-                    theme = "ivy",
-                    wrap_results = true,
-                    layout_config = {
-                        height = 0.4,
-                    },
-                    line_width = "full",
-                },
-            },
-            extensions = {
-                fzf = {
-                    fuzzy = true,
-                    override_generic_sorter = true,
-                    override_file_sorter = true,
-                }
-            }
-        }
+		telescope.setup({
+			defaults = {
+				path_display = { "smart" },
+				layout_config = {
+					width = 0.9,
+					height = 0.9,
+					preview_width = 0.6,
+				},
+				wrap_results = true,
+			},
+			pickers = {
+				diagnostics = {
+					theme = "ivy",
+					wrap_results = true,
+				},
+			},
+			extensions = {
+				fzf = {
+					fuzzy = true,
+					override_generic_sorter = true,
+					override_file_sorter = true,
+				},
+			},
+		})
 
-        local map = vim.keymap.set
-        map('n', '<leader>ff', require('telescope.builtin').find_files, { desc = 'Find Files' })
-        map('n', '<leader>fg', require('telescope.builtin').live_grep,  { desc = 'Live Grep' })
-        map('n', '<leader>fb', require('telescope.builtin').buffers,    { desc = 'Buffers' })
-        map('n', '<leader>fh', require('telescope.builtin').help_tags,  { desc = 'Help Tags' })
-        map('n', '<leader>fc', require('telescope.builtin').colorscheme,  { desc = 'Colorschemes' })
-        map('n', '<leader>fx', require('telescope.builtin').diagnostics,  { desc = 'Diagnostics List' })
-        map('n', '<leader>f.', require('telescope.builtin').oldfiles,  { desc = 'Old Files' })
-    end
+		local map = vim.keymap.set
+		map("n", "<leader>ff", require("telescope.builtin").find_files, { desc = "Find Files" })
+		map("n", "<leader>fg", require("telescope.builtin").live_grep, { desc = "Live Grep" })
+		map("n", "<leader>fb", require("telescope.builtin").buffers, { desc = "Buffers" })
+		map("n", "<leader>fh", require("telescope.builtin").help_tags, { desc = "Help Tags" })
+		map("n", "<leader>fc", function()
+			require("telescope.builtin").diagnostics({ severity_bound = 0 })
+		end, { desc = "Diagnostics List" })
+		map("n", "<leader>f.", require("telescope.builtin").oldfiles, { desc = "Old Files" })
+	end,
 }
