bdf8a8773001a36d1a64f001780e5cda74ccb062
cjamhub <cjamchen@gmail.com>
2025-10-07 14:31:06 +0100

    upgrade: remove markdown-render

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -128,13 +128,6 @@ return {
 					},
 				},
 			},
-			{
-				"MeanderingProgrammer/render-markdown.nvim",
-				opts = {
-					file_types = { "markdown", "Avante" },
-				},
-				ft = { "markdown", "Avante" },
-			},
 		},
 	},
 }
